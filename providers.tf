bc3f0c09ff52cf18f8942138d5da1a426c54b30a
himanshu <abc@gmail.com>
2023-09-01 12:47:44 +0530

    new

---
+++ providers.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-southeast-1"
+  access_key = var.accesskey
+  secret_key = var.secretkey
+}
+terraform {
+  cloud {
+    organization = "septterracloud"
+    workspaces {
+      name = "gitworkspace"
+    }
+  }
+}
+resource "aws_iam_user" "gitrepouser" {
+  name = "girrepouse"
+}
